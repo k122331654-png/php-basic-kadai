ef4c8659ec5e61f57dcceb3b00e47b2efb0ae121
k122331654-png <k122331654@gmail.com>
2025-12-25 17:02:20 +0900

    Add files via upload

---
+++ htdocs/Kadai_005/score.php
@@ -0,0 +1,23 @@
+    <p>
+        <?php
+        $score1= 80;
+        $score2= 50;
+        $score3= 55;
+        $score4= 40;
+        $score5= 25;
+        $score6= 25;
+        $score7= 80;
+        $score8= 90;
+        $score9= 35;
+        $score10= 60;
+        
+        $scoretotal= $score1+$score2+$score3+$score4+$score5+
+        $score6+$score7+$score8+$score9+$score10;
+        
+        $scoreaverrage=$scoretotal / 10;
+        echo $scoreaverrage;
+      ?> 
+      
+
+
+    </p>
